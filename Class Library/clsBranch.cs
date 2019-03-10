b5c0e81d60ceffe0bd8f2738731bee6e00184be8
atif50 <p17214929@my365.ac.dmu.uk>
2019-03-10 15:36:33 +0000

    Added more tests for Branch

--- Class Library/clsBranch.cs
+++ Class Library/clsBranch.cs
@@ -1,7 +1,26 @@
-﻿namespace Class_Library
+﻿using System;
+
+namespace Class_Library
 {
     public class clsBranch
     {
         public int BranchNo { get; set; }
+        public object Branch { get; set; }
+
+        public string Valid(string someBranch)
+        {
+            // if the name of the Branch is not blank
+            if (someBranch !="")
+            {
+                //return a blank string
+                return "";
+            }
+            else
+            {
+                //otherwise return an error message 
+                return "the Branch name may not be blank!";
+            }
+            
+        }
     }
 }
