da77405d3b4b4aede32feb07e9c4744cb7fd8ad9
BradWartho <p17212092@my365.dmu.ac.uk>
2019-03-21 19:13:09 +0000

    Updated presentation layer

--- VGFrontEnd/StaffDelete.aspx.designer.cs
+++ VGFrontEnd/StaffDelete.aspx.designer.cs
@@ -22,22 +22,22 @@ namespace VGFrontEnd {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// TextBoxStaffID control.
+        /// TextBox1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBoxStaffID;
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
         
         /// <summary>
-        /// ButtonDeleteStaff control.
+        /// ButtonDeleteStaffMember control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button ButtonDeleteStaff;
+        protected global::System.Web.UI.WebControls.Button ButtonDeleteStaffMember;
         
         /// <summary>
         /// lblError control.
